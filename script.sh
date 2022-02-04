5f14ef52e3172f6c0c00708a65cfb4a29117bac3
SmirnovAA <kyct_2018@mail.ru>
2022-02-04 13:10:13 +0300

    First commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+i=0
+gener=$RANDOM
+RANGE=100
+let "gener %= $RANGE"
+while [ $i -le 3 ]
+do
+i=$(( $i + 1 ))
+echo Enter number:
+read num 
+if [ $num -eq $gener ]
+then
+echo Yes!
+break
+else
+echo No!
+fi
+done
+echo Game over. $gener
